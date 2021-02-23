b52218092fb014bbb16199ed52a8fb9560db8147
Ke Ning <u7175553@anu.edu.au>
2021-02-23 14:03:14 +0800

    Finished lab exercises and passed all tests!

--- src/comp1110/lab1/HelloTutor.java
+++ src/comp1110/lab1/HelloTutor.java
@@ -17,11 +17,11 @@ public class HelloTutor extends Application {
         // TODO: your tutor's name.
 
         // Create the root stack pane
-        primaryStage.setTitle("Hello _");
+        primaryStage.setTitle("Hello Josh");
         StackPane root = new StackPane();
 
         // Create label and add to root
-        Label helloLabel = new Label("Hello _!");
+        Label helloLabel = new Label("Hello Josh!");
         root.getChildren().add(helloLabel);
 
         // Set scene of stage with size 240x150 and show the stage
