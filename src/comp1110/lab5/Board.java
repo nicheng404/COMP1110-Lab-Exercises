c545064b7b9b8ffa72c14e350a5b868ba7752dd6
Ke Ning <u7175553@anu.edu.au>
2021-04-19 22:24:02 +0800

    Journal week6

---
+++ src/comp1110/lab5/Board.java
@@ -0,0 +1,10 @@
+package comp1110.lab5;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+    public void start(Stage primaryStage) {
+
+    }
+}
